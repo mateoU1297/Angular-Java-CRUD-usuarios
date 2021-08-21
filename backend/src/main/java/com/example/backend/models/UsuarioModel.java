6dd89ad8f6232ca2df8dc926db5149364968ee7c
mateoU1297 <castro.mateo@correounivalle.edu.co>
2021-08-20 23:25:36 -0500

    se agrega el backend

---
+++ backend/src/main/java/com/example/backend/models/UsuarioModel.java
@@ -0,0 +1,48 @@
+package com.example.backend.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "usuario")
+public class UsuarioModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+    private String nombre;
+    private Boolean activo;
+    private Integer id_rol;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Boolean getActivo() {
+        return activo;
+    }
+
+    public void setActivo(Boolean activo) {
+        this.activo = activo;
+    }
+
+    public Integer getIdRol() {
+        return id_rol;
+    }
+
+    public void setIdRol(Integer idRol) {
+        this.id_rol = idRol;
+    }
+}
